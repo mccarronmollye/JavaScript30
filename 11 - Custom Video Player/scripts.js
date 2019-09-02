e4fb1dd278ad3f92a97e51d8a0656e7088452ce9
Molly McCarron <mccarronmollye@gmail.com>
2019-09-01 21:46:00 -0400

    progress bar correlates with video duration

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -27,10 +27,21 @@ function handleRangeUpdate() {
     video[this.name] = this.value
 }
 
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    console.log(e)    
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('click', handleRangeUpdate))
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate))
+progress.addEventListener('click', scrub);
